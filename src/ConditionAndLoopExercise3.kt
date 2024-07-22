b9983295b9edd55a533390f314b5daeff7c8fc75
aweeshathavishanka <aweesha.info@gmail.com>
2024-07-22 10:16:08 +0530

    Update Condition Loop Exercie

--- src/ConditionAndLoopExercise3.kt
+++ src/ConditionAndLoopExercise3.kt
@@ -9,11 +9,34 @@ fun calculateGrade(mark: Int): String {
     }
 }
 
+fun calculateGradesIf(mark: Int): String {
+    return if (mark in 75..100) {
+        "A"
+    } else if (mark in 65..74) {
+        "B"
+    } else if (mark in 50..64) {
+        "C"
+    } else if (mark in 35..49) {
+        "D"
+    } else if (mark in 0..34) {
+        "F"
+    } else {
+        "Invalid mark"
+    }
+}
+
 fun main() {
     val marks = listOf(85, 70, 55, 40, 20, -5, 105)
 
+    // Using the when expression
     for (mark in marks) {
         val grade = calculateGrade(mark)
+        println("Mark: $mark - Grade: $grade")
+    }
+
+    // Using the if-else expression
+    for (mark in marks) {
+        val grade = calculateGradesIf(mark)
         println("Mark: $mark - Grade: $grade")
     }
 }
